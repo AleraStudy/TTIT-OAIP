751698bfe0eb5f19249e078f53cbd6007b765c5a
jiexa <test@test.com>
2023-12-10 01:32:17 +0700

    lw4 update

--- lw4/main.cpp
+++ lw4/main.cpp
@@ -4,26 +4,32 @@
 
 void removeDuplicates(char text[]) {
     std::map<char, bool> seen;
-    int length = 0;
+    int readIndex = 0;
     int writeIndex = 0;
 
-    while (text[length] != '\0') {
-        if (seen.find(text[length]) == seen.end()) {
-            seen[text[length]] = true;
-            text[writeIndex++] = text[length];
+    while (text[readIndex] != '\0') {
+        auto it = seen.find(text[readIndex]);
+        auto end = seen.end();
+        const bool isCharacterUnique = it == end;
+
+        if (isCharacterUnique) {
+            seen[text[readIndex]] = true;
+            text[writeIndex] = text[readIndex];
+            writeIndex++;
         }
-        length++;
+        readIndex++;
     }
 
     text[writeIndex] = '\0';
 }
 
+
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     setlocale(LC_ALL,"ru-RU");
 
-    const int max_length = 100;
-    char text[max_length] = {}; // Array to read the string
+    constexpr int max_length = 100;
+    char text[max_length] = {};
 
     std::cout << "Введите текст (не более 100 символов):" << std::endl;
     std::cin.getline(text, max_length);
