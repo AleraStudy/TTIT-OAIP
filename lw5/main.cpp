09138c54eb2a015a007da681456d87c191f387d2
jiexa <test@test.com>
2023-12-10 01:55:45 +0700

    lw5 update to camelCase

--- lw5/main.cpp
+++ lw5/main.cpp
@@ -9,20 +9,20 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
     setlocale(LC_ALL,"ru-RU");
 
-    int N, M;
+    int n, m;
     cout << "Введите размеры матрицы N и M: ";
-    cin >> N >> M;
+    cin >> n >> m;
 
     // Dynamic memory allocation
-    const auto matrix = new int*[N];
-    for (int i = 0; i < N; i++) {
-        matrix[i] = new int[M];
+    const auto matrix = new int*[n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[m];
     }
 
     // Fill the matrix with random numbers
     cout << "Исходная матрица:" << endl;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             matrix[i][j] = rand() % 100;
             cout << setw(4) << matrix[i][j];
         }
@@ -30,31 +30,31 @@ int main()
     }
 
     // Find the maximum below the secondary diagonal and the minimum above the main diagonal
-    int max_below_secondary_diag = INT_MIN;
-    int min_above_main_diag = INT_MAX;
+    int maxBelowSecondaryDiag = INT_MIN;
+    int minAboveMainDiag = INT_MAX;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             // Below the secondary diagonal
-            if (i + j > M-1) {
-                if (matrix[i][j] > max_below_secondary_diag) {
-                    max_below_secondary_diag = matrix[i][j];
+            if (i + j > m - 1) {
+                if (matrix[i][j] > maxBelowSecondaryDiag) {
+                    maxBelowSecondaryDiag = matrix[i][j];
                 }
             }
             // Above the main diagonal
             if (j > i) {
-                if (matrix[i][j] < min_above_main_diag) {
-                    min_above_main_diag = matrix[i][j];
+                if (matrix[i][j] < minAboveMainDiag) {
+                    minAboveMainDiag = matrix[i][j];
                 }
             }
         }
     }
 
-    cout << "Максимальный элемент ниже побочной диагонали: " << max_below_secondary_diag << endl;
-    cout << "Минимальный элемент выше главной диагонали: " << min_above_main_diag << endl;
+    cout << "Максимальный элемент ниже побочной диагонали: " << maxBelowSecondaryDiag << endl;
+    cout << "Минимальный элемент выше главной диагонали: " << minAboveMainDiag << endl;
 
     // Free memory
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < n; i++) {
         delete[] matrix[i];
     }
     delete[] matrix;
